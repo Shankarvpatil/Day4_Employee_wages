88bf1d2ea39d52f1046cc77350b3c7ba7c52c3ab
Shankar Vilas Patil <69981881+Shankarvpatil@users.noreply.github.com>
2021-06-24 11:59:13 +0530

    Update Employee_wages.java

--- Employee_wages.java
+++ Employee_wages.java
@@ -1,34 +1,39 @@
-package uc5_Calculate_Emp_Wage_For_Month;
+package uc6_Emp_mothly_wage_hr_day_conditon;
 
 import uc4_Wager_Using_Switch_Case.SwitchCase;
 
-public class Monthly_Wages extends SwitchCase {
-	
+public class Montly_Wage extends SwitchCase {
+
 	public static final int isFulltime = 1, isParttime = 2;
 	
 	public static void main(String[] args) {
 		
-		int TotalWage = 0;
+		int TotalWage = 0, day = 0, WorkinHr = 0;
 		
-		for(int Day = 1; Day <= 20; Day++) {
+		while( day <= 20 || WorkinHr <= 100 ) {
 		
 			int check = Switch();
 			
 			switch (check) {
 			
-			case isFulltime:   // full time wage
+			case isFulltime:                                                        // full time wage
 				
+				day++;
+				WorkinHr += 8;
 				TotalWage += Wager();     // Daily wager
 				break;
 				
 			case isParttime:                                                        // part time wage
-			
+				
+				day++;
+				WorkinHr += 4;
 				TotalWage += PartTimeWage();
 				break;
 				
-			default:                                                    //Absent
-
-              			TotalWage += 0;
+			default: 																	//Absent
+				day++;
+				WorkinHr += 0;
+                		TotalWage += 0;
 			}
 		}
 
